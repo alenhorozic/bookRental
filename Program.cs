7f052254ba9b21e518ff11fd3410b60c1b11abb4
Alen Horozic <alenhorozic@hotmail.com>
2020-12-13 14:15:51 +0100

    fix print message

--- Program.cs
+++ Program.cs
@@ -10,24 +10,26 @@ namespace bookRental
             
             var member = new Member();
 
-            
+            Console.WriteLine("\n***********Create Rental with 2 book**************");
             member.SetCommandOption(1 /*Add*/);
             member.SetBook(new Book("C# in NET Core", "Gaurar Arorra", 350, "2021-01-15"));
             member.RunCommand();
 
             member.SetCommandOption(1 /*Add*/);
-            member.SetBook(new Book("ASP.net", "Mark J Price", 478, "2021-02-05"));
+            member.SetBook(new Book("ASP.net", "Mark J Price", 478, "2021-01-15"));
             member.RunCommand();
             member.ShowRental();
 
             SetMemberCard("Free", "Student");
 
+            Console.WriteLine("\n***********Redo 1 book and change memberCard**************");
             member.SetCommandOption(2 /*Redo*/);
             member.RunCommand();
             member.ShowRental();
 
             SetMemberCard("Free", "Senior");
 
+            Console.WriteLine("\n***********Undo 1 book and change memberCard**************");
             member.SetCommandOption(3 /*Undo*/);
             member.RunCommand();
             member.ShowRental();
